bc57290ac2bbb1d2bf9023f6f87454b5c635c81f
devchas <devchas@gmail.com>
2016-03-06 01:10:20 -0500

    updated styling

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   post 'apartments', to: 'apartments#create'
-  get 'apartments', to: 'apartments#index'
+  #get 'apartments', to: 'apartments#index'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
